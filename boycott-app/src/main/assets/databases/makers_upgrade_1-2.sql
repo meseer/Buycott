c73bf786eb3007de9f7aad3dfc3243f6873ce8bf
Mykhailo Delegan <meseer@gmail.com>
2014-02-10 14:00:35 +0200

    Update product database

--- boycott-app/src/main/assets/databases/makers_upgrade_1-2.sql
+++ boycott-app/src/main/assets/databases/makers_upgrade_1-2.sql
@@ -6,10 +6,10 @@ INSERT INTO makers (Barcode, CountryCode, MakerCode, Maker, Title)
 VALUES("4823002200000", 482, 30022, "Фармак", "Медикаменти")
 
 INSERT INTO makers (Barcode, CountryCode, MakerCode, Maker, Title)
-VALUES("4824024000000", 482, 40240, "Повна чаша", "Продукти")
+VALUES(4824024000000, 482, 40240, "Повна чаша", "Продукти")
 
 INSERT INTO makers (Barcode, CountryCode, MakerCode, Maker, Title)
-VALUES("8594910012403", 859, 49100, "CARLOLACIN", "Массажный крем с экстрактом зеленого чая CARLOLACIN")
+VALUES(8594910012403, 859, 49100, "CARLOLACIN", "Массажный крем с экстрактом зеленого чая CARLOLACIN")
 
 -- INSERT INTO makers (Barcode, CountryCode, MakerCode, Maker, Title) VALUES("4824035006877", 482, 40350, "", "")
 -- INSERT INTO makers (Barcode, CountryCode, MakerCode, Maker, Title) VALUES("4820177030039", 482, 1770, "", "")
